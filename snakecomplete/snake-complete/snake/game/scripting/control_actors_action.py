19e827978cc5cca821915c938a9571dab0b90f99
Romi27 <romialdana27@gmail.com>
2022-06-23 11:16:55 -0300

    conrol

--- snakecomplete/snake-complete/snake/game/scripting/control_actors_action.py
+++ snakecomplete/snake-complete/snake/game/scripting/control_actors_action.py
@@ -1,3 +1,4 @@
+from itertools import cycle
 import constants
 from game.scripting.action import Action
 from game.shared.point import Point
@@ -45,5 +46,21 @@ class ControlActorsAction(Action):
         if self._keyboard_service.is_key_down('s'):
             self._direction = Point(0, constants.CELL_SIZE)
         
-        snake = cast.get_first_actor("snakes")
-        snake.turn_head(self._direction)
+        cycle = cast.get_first_actor("snakes")
+        cycle.turn_head(self._direction)
+
+        # left
+        if self._keyboard_service.is_key_down('j'):
+            self._direction = Point(-constants.CELL_SIZE, 0)
+
+        # right
+        if self._keyboard_service.is_key_down('l'):
+            self._direction = Point(constants.CELL_SIZE, 0)
+
+        # up
+        if self._keyboard_service.is_key_down('i'):
+            self._direction = Point(0, -constants.CELL_SIZE)
+
+        # down
+        if self._keyboard_service.is_key_down('k'):
+            self._direction = Point(0, constants.CELL_SIZE)
