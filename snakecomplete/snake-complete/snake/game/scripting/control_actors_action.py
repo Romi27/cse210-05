e72d773043faa4882074142ca5e7413483523d66
Romi27 <97213039+Romi27@users.noreply.github.com>
2022-06-26 21:00:41 -0300

    Update control_actors_action.py

--- snakecomplete/snake-complete/snake/game/scripting/control_actors_action.py
+++ snakecomplete/snake-complete/snake/game/scripting/control_actors_action.py
@@ -8,7 +8,6 @@ class ControlActorsAction(Action):
     An input action that controls the snake.
     
     The responsibility of ControlActorsAction is to get the direction and move the snake's head.
-
     Attributes:
         _keyboard_service (KeyboardService): An instance of KeyboardService.
     """
@@ -20,43 +19,51 @@ class ControlActorsAction(Action):
             keyboard_service (KeyboardService): An instance of KeyboardService.
         """
         self._keyboard_service = keyboard_service
-        self._direction = Point(constants.CELL_SIZE, 0)
-        
-    def set_keys(self, players, index):
-        player = players[index]
-        keyboard = player.get_keyboard()
-        left = keyboard[0]
-        right = keyboard[1]
-        up = keyboard[2]
-        down = keyboard[3]
-        # left
-        if self._keyboard_service.is_key_down(left):
-                self._direction = Point(-constants.CELL_SIZE, 0)
-        
-         # right
-        if self._keyboard_service.is_key_down(right):
-            self._direction = Point(constants.CELL_SIZE, 0)
-            
-        # up
-        if self._keyboard_service.is_key_down(up):
-            self._direction = Point(0, -constants.CELL_SIZE)
-            
-        # down
-        if self._keyboard_service.is_key_down(down):
-            self._direction = Point(0, constants.CELL_SIZE)
-            
-        player.turn_head(self._direction)
+        self._direction = Point(0, -1*constants.CELL_SIZE)
 
-
+        self._direction2 = Point(0, -1*constants.CELL_SIZE)
 
     def execute(self, cast, script):
         """Executes the control actors action.
-
         Args:
             cast (Cast): The cast of Actors in the game.
             script (Script): The script of Actions in the game.
         """
-        players = [cast.get_first_actor("player1"), cast.get_first_actor("player2")]
-        for i in range(2):
-            index = i
-            self.set_keys(players, index)
+        # left
+        if self._keyboard_service.is_key_down('a'):
+            self._direction = Point(-constants.CELL_SIZE, 0)
+
+        # right
+        if self._keyboard_service.is_key_down('d'):
+            self._direction = Point(constants.CELL_SIZE, 0)
+
+        # up
+        if self._keyboard_service.is_key_down('w'):
+            self._direction = Point(0, -constants.CELL_SIZE)
+
+        # down
+        if self._keyboard_service.is_key_down('s'):
+            self._direction = Point(0, constants.CELL_SIZE)
+
+        cycle= cast.get_first_actor("player1")
+        cycle.turn_head(self._direction)
+
+        # I changed _direction to _direction2 here
+        # left
+        if self._keyboard_service.is_key_down('j'):
+            self._direction2 = Point(-constants.CELL_SIZE, 0)
+
+        # right
+        if self._keyboard_service.is_key_down('l'):
+            self._direction2 = Point(constants.CELL_SIZE, 0)
+
+        # up
+        if self._keyboard_service.is_key_down('i'):
+            self._direction2 = Point(0, -constants.CELL_SIZE)
+
+        # down
+        if self._keyboard_service.is_key_down('k'):
+            self._direction2 = Point(0, constants.CELL_SIZE)
+
+        cycle = cast.get_first_actor("player2")
+        cycle.turn_head(self._direction2)
