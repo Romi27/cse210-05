5e9e477aeca4e2e45bfa997c440ebce9ec733fbe
Romi27 <97213039+Romi27@users.noreply.github.com>
2022-06-26 21:01:49 -0300

    Update handle_collisions_action.py

--- snakecomplete/snake-complete/snake/game/scripting/handle_collisions_action.py
+++ snakecomplete/snake-complete/snake/game/scripting/handle_collisions_action.py
@@ -93,3 +93,4 @@ class HandleCollisionsAction(Action):
                 for segment in segments:
                     segment.set_color(constants.WHITE)
                 food.set_color(constants.WHITE)
+
